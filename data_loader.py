21618c1e884b6d814ed539c6fe8f5533e58d108c
jojonki <jonki@jonki.net>
2017-02-05 21:18:32 +0900

    not use specific lang name

--- data_loader.py
+++ data_loader.py
@@ -3,29 +3,29 @@ import codecs
 
 class DataLoader:
     @staticmethod
-    def load(ja_file, en_file):
-        jvocab = {}
-        jlines = codecs.open(ja_file, 'r', 'utf-8').read().split('\n')
-        for i in range(len(jlines)):
-            lt = jlines[i].split()
+    def load(src_file, dst_file):
+        src_vocab = {}
+        src_lines = codecs.open(src_file, 'r', 'utf-8').read().split('\n')
+        for i in range(len(src_lines)):
+            lt = src_lines[i].split()
             for w in lt:
-                if w not in jvocab:
-                    jvocab[w] = len(jvocab)
-        jvocab['<eos>'] = len(jvocab)
+                if w not in src_vocab:
+                    src_vocab[w] = len(src_vocab)
+        src_vocab['<eos>'] = len(src_vocab)
 
-        evocab = {}
-        id2enword = {}
-        elines = codecs.open(en_file, 'r', 'utf-8').read().split('\n')
-        for i in range(len(elines)):
-            lt = elines[i].split()
+        dst_vocab = {}
+        id2word = {}
+        dst_lines = codecs.open(dst_file, 'r', 'utf-8').read().split('\n')
+        for i in range(len(dst_lines)):
+            lt = dst_lines[i].split()
             for w in lt:
-                if w not in evocab:
-                    id = len(evocab)
-                    evocab[w] = id
-                    id2enword[id] = w
+                if w not in dst_vocab:
+                    id = len(dst_vocab)
+                    dst_vocab[w] = id
+                    id2word[id] = w
 
-        id = len(evocab)
-        evocab['<eos>'] = id
-        id2enword[id] = '<eos>'
+        id = len(dst_vocab)
+        dst_vocab['<eos>'] = id
+        id2word[id] = '<eos>'
 
-        return jvocab, jlines, evocab, elines, id2enword
+        return src_vocab, src_lines, dst_vocab, dst_lines, id2word
